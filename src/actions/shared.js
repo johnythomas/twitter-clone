b43c472c5e8d23f74fb57c73a3c0ef80582e85eb
johnythomas <johnythomas007@gmail.com>
2018-04-26 10:23:48 +0530

    Fetch initial data in app component

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -7,7 +7,7 @@ const AUTHED_ID = "tylermcginnis"
 
 export function handleInitialData() {
   return dispatch =>
-    getInitialData.then(({ users, tweets }) => {
+    getInitialData().then(({ users, tweets }) => {
       dispatch(receiveUsers(users))
       dispatch(receiveTweets(tweets))
       dispatch(setAuthedUser(AUTHED_ID))

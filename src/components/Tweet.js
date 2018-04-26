e3702bfae915b10cd30073608f62078d09259bd3
johnythomas <johnythomas007@gmail.com>
2018-04-27 00:47:55 +0530

    Add like functionality

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -4,6 +4,7 @@ import TiArrowBackOutline from "react-icons/lib/ti/arrow-back-outline"
 import TiHeartOutline from "react-icons/lib/ti/heart-outline"
 import TiHeartFullOutline from "react-icons/lib/ti/heart-full-outline"
 import { formatTweet, formatDate } from "../utils/helpers"
+import { handleToggleTweet } from "../actions/tweets"
 
 class Tweet extends Component {
   toParent = (e, id) => {
@@ -13,6 +14,14 @@ class Tweet extends Component {
 
   handleLike = e => {
     e.preventDefault()
+    const { dispatch, tweet, authedUser } = this.props
+    dispatch(
+      handleToggleTweet({
+        id: tweet.id,
+        hasLiked: tweet.hasLiked,
+        authedUser
+      })
+    )
   }
 
   render() {
